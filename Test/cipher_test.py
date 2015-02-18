9e63abb638c20f410cc5aeb77312d020e6fa220e
juliencandela <julien.candela@centrale-marseille.fr>
2015-02-18 09:23:20 +0100

    test caractere inconnu

--- Test/cipher_test.py
+++ Test/cipher_test.py
@@ -7,8 +7,14 @@ __author__ = 'julien'
 
 class TestCipher(unittest.TestCase):
 
-    def test_cipher(self):
+    def test_cipher_simple_letter(self):
         encoder = Encoder(1)
         text = "A"
         encoding_text = encoder.cipher(text)
         self.assertEqual(encoding_text, "B")
+
+    def test_cipher_space(self):
+        encoder = Encoder(1)
+        text = "A B"
+        encoding_text = encoder.cipher(text)
+        self.assertEqual(encoding_text, "B C")
--- src/encoder.py
+++ src/encoder.py
@@ -11,6 +11,9 @@ class Encoder :
     def cipher(self, text):
         cipher_text = ""
         for letter in text:
-            indice = self.alphabet.index(letter)
-            cipher_text += self.alphabet[indice+self.key % len(self.alphabet)]
+            if letter in self.alphabet :
+                indice = self.alphabet.index(letter)
+                cipher_text += self.alphabet[indice+self.key % len(self.alphabet)]
+            else:
+                cipher_text += letter
         return cipher_text
