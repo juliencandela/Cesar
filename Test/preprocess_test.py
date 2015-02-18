47c383f459296c88faa0a87b634f057858dbe1bd
juliencandela <julien.candela@centrale-marseille.fr>
2015-02-18 10:21:32 +0100

    test import texte refactor

--- Test/preprocess_test.py
+++ Test/preprocess_test.py
@@ -1,13 +1,14 @@
 import unittest
-from Preprocess import Preprocess
+from preprocess import Preprocess
 
 __author__ = 'marie-helene'
+
 
 class TestCipher(unittest.TestCase):
 
     def test_preprocess(self):
         url = "https://www.gutenberg.org/cache/epub/28210/pg28210.txt"
-        preprocess_machine = Preprocess(url)
-        imported_text = preprocess_machine.import_text()
+        preprocess_machine = Preprocess()
+        imported_text = preprocess_machine.import_text(url)
         self.assertEqual(imported_text[0], "T")
 
---
+++ src/preprocess.py
@@ -0,0 +1,13 @@
+import requests
+
+__author__ = 'marie-helene'
+
+
+class Preprocess:
+
+    def __init__(self):
+        pass
+
+    def import_text(self, url):
+        import_text = requests.get(url)
+        return import_text.text
