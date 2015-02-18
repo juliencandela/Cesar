5afd9546ab38e3d9e90a9567694e43cdb2e5b32e
juliencandela <julien.candela@centrale-marseille.fr>
2015-02-18 09:19:00 +0100

    test A devient B

--- src/encoder.py
+++ src/encoder.py
@@ -1,9 +1,16 @@
+import string
+
 __author__ = 'marie-helene'
 
 class Encoder :
 
     def __init__(self, key=0):
         self.key = key
+        self.alphabet = list(string.ascii_uppercase)
 
     def cipher(self, text):
-        return "B"
+        cipher_text = ""
+        for letter in text:
+            indice = self.alphabet.index(letter)
+            cipher_text += self.alphabet[indice+self.key % len(self.alphabet)]
+        return cipher_text
